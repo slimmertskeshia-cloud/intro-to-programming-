cbfe03c482c1a639a378829144fba718051222e9
slimmertskeshia-cloud <slimmertskeshia@gmail.com>
2026-01-27 20:59:00 +0200

    Add files via upload

---
+++ book 1/Chapter_6/Class-Work/B129&L102.js
@@ -0,0 +1,8 @@
+console.log("--------------DO WHILE LOOP-------")
+let number = 3;  // Start with number 3
+
+do {
+    console.log(number); // Print the current number
+    number++;            // Add 3 to the number
+} while (number <= 10000);   // Keep going while number is 10000 or less
+
