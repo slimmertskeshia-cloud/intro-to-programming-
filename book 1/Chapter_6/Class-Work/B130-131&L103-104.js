ddeec5a780ca7cd94ec1b172ac06f797cbdf40f7
slimmertskeshia-cloud <slimmertskeshia@gmail.com>
2026-01-27 21:30:19 +0200

    Add files via upload

---
+++ book 1/Chapter_6/Class-Work/B130-131&L103-104.js
@@ -0,0 +1,17 @@
+console.log("---------------FOR LOOP WITH ARRAY-------")
+var animals = ["oranges", "grapes", "potatos", "tomatos"];  // A list of fruits and vegetables
+for (var i = 0; i < animals.length; i++) {                            // Loop through each animal in the list
+    console.log("This farm contains some " + animals[i] + ".", '\n');           // Print the current animal
+}
+console.log("---------------FOR LOOP-------")
+for (var oranges = 50; oranges < 220; oranges++) {  // Start counting from 0, repeat until 20
+    console.log("I have counted " + oranges + " oranges!");  // Print the current count of oranges
+}
+console.log("Zzzzzzzzzzz", '\n');                                     // Print the message
+
+console.log("---------------WHILE LOOP-------")
+var oranges = 50;         // Start counting from 0
+while (oranges < 220) {  // Continue while oranges is less than 20
+    console.log("I have found " + oranges + " oranges!");  // Print the current count
+    oranges++;           // Increase the count by 1
+}
