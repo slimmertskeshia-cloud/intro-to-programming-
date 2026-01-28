743e758e90defff60d6581151959fa60894f1227
slimmertskeshia-cloud <slimmertskeshia@gmail.com>
2026-01-28 09:52:20 +0200

    Refactor console logs in loop examples

--- book 1/Chapter_6/Class-Work/B130-131&L103-104.js
+++ book 1/Chapter_6/Class-Work/B130-131&L103-104.js
@@ -1,7 +1,7 @@
 console.log("---------------FOR LOOP WITH ARRAY-------")
 var animals = ["oranges", "grapes", "potatos", "tomatos"];  // A list of fruits and vegetables
 for (var i = 0; i < animals.length; i++) {                            // Loop through each animal in the list
-    console.log("This farm contains some " + animals[i] + ".", '\n');           // Print the current animal
+    console.log("awesome " + animals[i] + ".", '\n');           // Print the current animal
 }
 console.log("---------------FOR LOOP-------")
 for (var oranges = 50; oranges < 220; oranges++) {  // Start counting from 0, repeat until 20
@@ -14,4 +14,5 @@ var oranges = 50;         // Start counting from 0
 while (oranges < 220) {  // Continue while oranges is less than 20
     console.log("I have found " + oranges + " oranges!");  // Print the current count
     oranges++;           // Increase the count by 1
+
 }
